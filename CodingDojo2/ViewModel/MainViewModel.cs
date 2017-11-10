d681613bc2f927de9c07589f62b98279033d2cd2
wi16b067 <wi16b067@DESKTOP-DIA8AHQ>
2017-11-10 12:39:03 +0100

    pretty final commit

--- CodingDojo2/ViewModel/MainViewModel.cs
+++ CodingDojo2/ViewModel/MainViewModel.cs
@@ -1,4 +1,11 @@
+using CodingDojo2.DataSim;
 using GalaSoft.MvvmLight;
+using Shared.BaseModels;
+using Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Threading;
 
 namespace CodingDojo2.ViewModel
 {
@@ -16,19 +23,70 @@ namespace CodingDojo2.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
+        private Simulator sim;
+        private List<ItemVm> modelItems = new List<ItemVm>();
+        public ObservableCollection<ItemVm> SensorList { get; set; }
+        public ObservableCollection<ItemVm> ActorList { get; set; }
+        public ObservableCollection<string> ModeSelectionList { get; set; }
+
+        private string curDate= System.DateTime.Now.ToLocalTime().ToShortDateString();
+        
+        public string CurDate
+        {
+            get { return curDate; }
+            set { curDate = value; RaisePropertyChanged("CurDate"); }
+        }
+
+        private string curTime = System.DateTime.Now.ToLocalTime().ToShortTimeString();
+
+        public string CurTime
+        {
+            get { return curTime; }
+            set { curTime = value; RaisePropertyChanged("CurTime"); }
+        }
+
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            SensorList = new ObservableCollection<ItemVm>();
+            ActorList = new ObservableCollection<ItemVm>();
+            ModeSelectionList = new ObservableCollection<string>();
+
+            foreach (var item in Enum.GetNames(typeof(SensorModeType)))
+            {
+                ModeSelectionList.Add(item);
+            }
+            foreach (var item in Enum.GetNames(typeof(ModeType)))
+            {
+                ModeSelectionList.Add(item);
+
+            }
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new System.TimeSpan(0, 0, 3);
+            timer.Tick += Timer_Tick;
+
+            timer.Start();
+
+            LoadData();
         }
+
+        private void LoadData()
+        {
+            Simulator sim = new Simulator(modelItems);
+            foreach (var item in sim.Items)
+            {
+                if (item.ItemType.Equals(typeof(ISensor)))
+                    SensorList.Add(item);
+                else if (item.ItemType.Equals(typeof(IActuator)))
+                    ActorList.Add(item);
+            }
+
+        }
+
+        private void Timer_Tick(object sender, System.EventArgs e)
+        {
+            CurDate = System.DateTime.Now.ToLocalTime().ToShortDateString();
+            CurTime = System.DateTime.Now.ToLocalTime().ToShortTimeString();
+    }
     }
 }
